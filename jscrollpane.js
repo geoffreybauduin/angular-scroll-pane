0a53a93f7da88c4a95dc8f5ce384851eae33ed14
Geoffrey Bauduin <bauduin.geo@gmail.com>
2014-08-24 19:40:36 +0200

    wip

--- jscrollpane.js
+++ jscrollpane.js
@@ -1,21 +1,23 @@
-angular
-	.module('app')
-	.directive('scrollPane', function () {
-
-		return {
-			restrict: 'A',
-			transclude: true,
-			template: '<div class="scroll-pane"><div ng-transclude></div></div>',
-			link: function (scope, elem, $attrs) {
-				var config = {};
-				if ($attrs.scrollConfig) {
-					config = scope.$eval($attrs.scrollConfig);
-				}
-				jQuery(function () {
-					jQuery(".scroll-pane").jScrollPane(config);
-		      	});
-			},
-			replace: true
+angular.module("ngJScrollPane", []);
+angular.module("ngJScrollPane").directive("scrollPane", function () {
+	return {
+		restrict: 'A',
+		transclude: true,
+		template: '<div class="scroll-pane" ng-class="class" ng-id="id"><div ng-transclude></div></div>',
+		scope: {
+			class: "@",
+			id: "@"
 		}
+		link: function (scope, elem, $attrs) {
+			var config = {};
+			if ($attrs.scrollConfig) {
+				config = scope.$eval($attrs.scrollConfig);
+			}
+			jQuery(function () {
+				jQuery("#" + scope.id).jScrollPane(config);
+	      	});
+		},
+		replace: true
+	}
 
-	});
+});
